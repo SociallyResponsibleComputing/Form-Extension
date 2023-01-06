7f7571a75df7add570b7d92128199128805fcbea
Alexander Pena <arpenacoding@gmail.com>
2023-01-06 10:40:44 -0800

    added node modules to ignored

--- GAS/GetResponses.js
+++ GAS/GetResponses.js
@@ -1,14 +1,16 @@
 function onSubmit(e) {
-  const fileId = getResponses()
-  PutFile(DriveApp.getFileById(fileId))
+  const response = getFormResponse()
+  //PutFile(response.fileId)
+  //putJson(response)
   return `fileId: ${fileId}, fileName: ${file.getName()}`
 }
 
-function getResponses() {
+function getFormResponse() {
   let form = FormApp.getActiveForm()
   let allResponses = form.getResponses()
   let latestResponse = allResponses[allResponses.length - 1]
   let response = latestResponse.getItemResponses()
+  Logger.log(response)
   let responseDict = {}
   for (var i = 0; i < response.length; i++) {
     let question = response[i].getItem().getTitle()
@@ -17,5 +19,5 @@ function getResponses() {
   }
   Logger.log(`response Dict ${JSON.stringify(responseDict)}`)
   let fileId = responseDict['Submit Document'][0]
-  return fileId
+  return responseDict
 }
