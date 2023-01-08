e742900a0b24fbd13882c9e3f105db61bd318a25
Alexander Pena <arpenacoding@gmail.com>
2023-01-07 22:46:32 -0800

    merging GetFileFromGithub.sj into githubAPI.js

--- scripts/githubAPI.js
+++ scripts/githubAPI.js
@@ -1,4 +1,3 @@
-
 /* Parameters:
   file: the file object not the ID
 */
@@ -7,50 +6,93 @@ function putFile(file) {
   let blob = file.getBlob()
   const base64Str = Utilities.base64Encode(blob.getBytes())
   const bodyObj = {
-    "message": "Pushed file from GAS",
-    "content": base64Str
-}
-  const headers = {
-    'Authorization': `Bearer ${getGitToken()}`,
-    "Content-Type": "application/vnd.github.v3+json",
+    message: 'Pushed file from GAS',
+    content: base64Str,
   }
-    var options = {
-        'method': "PUT",
-        'headers': headers,
-        'payload': JSON.stringify(bodyObj),
-        //"muteHttpExceptions" : true,
-    }
-    const URL = `${REPO_DOCUMENTS}/${file.getName()}`
-    Logger.log(`URL: ${URL}`)
-    try {
-      UrlFetchApp.fetch(URL, options);
-    } catch (err) {
-      Logger.log(err)
-      Logger.log('Error! File probably exists already')
-    }
+  const headers = {
+    Authorization: `Bearer ${getGitToken()}`,
+    'Content-Type': 'application/vnd.github.v3+json',
+  }
+  var options = {
+    method: 'PUT',
+    headers: headers,
+    payload: JSON.stringify(bodyObj),
+    //"muteHttpExceptions" : true,
+  }
+  const URL = `${DOCUMENTS_URL}/${file.getName()}`
+  Logger.log(`URL: ${URL}`)
+  try {
+    UrlFetchApp.fetch(URL, options)
+  } catch (err) {
+    Logger.log(err)
+    Logger.log('Error! File probably exists already')
+  }
 }
 
+/* Parameters:
+  inputStr: The string to add to file
+*/
 function putString(inputStr) {
   let blob = Utilities.newBlob(inputStr)
   const base64Str = Utilities.base64Encode(blob.getBytes())
   const bodyObj = {
-    "message": "Put a string using GITHUB Rest API from doc_organizer",
-    "content": base64Str
+    message: 'Put a string using GITHUB Rest API from doc_organizer',
+    content: base64Str,
   }
   const headers = {
-    'Authorization': `Bearer ${getGitToken()}`,
-    "Content-Type": "application/vnd.github.v3+json",
+    Authorization: `Bearer ${getGitToken()}`,
+    'Content-Type': 'application/vnd.github.v3+json',
   }
-    var options = {
-        'method': "PUT",
-        'headers': headers,
-        'payload': JSON.stringify(bodyObj),
-        //"muteHttpExceptions" : true,
-    }
-    const URL = `${REPO_DOCUMENTS}/test.txt`
-    UrlFetchApp.fetch(URL, options);
+  var options = {
+    method: 'PUT',
+    headers: headers,
+    payload: JSON.stringify(bodyObj),
+    //"muteHttpExceptions" : true,
+  }
+  const URL = `${DOCUMENTS_URL}/test.txt`
+  return UrlFetchApp.fetch(URL, options)
+  //return URL
 }
 
-function putJson() {
-
+/* Google script file associated with grabbing files
+from Github
+Params:
+  filepath: path with '/' included in beginning
+ */
+function getFileFromGithub(filepath) {
+  var urlFetchOptions = {
+  "method": "GET",
+  "headers": {
+    "Accept": "application/vnd.github.raw",
+    "X-GitHub-Api-Version": "2022-11-28",
+    "Authorization": `Bearer ${getGitToken()}`
+  },
+  "muteHttpExceptions": false
+  }
+  const url = DOCUMENTS_URL + filepath
+  Logger.log('url ' + url)
+  const gitResponse = UrlFetchApp.fetch(url, urlFetchOptions)
+  return gitResponse
 }
+
+/* Google script file associated with grabbing files
+from Github */
+function getReadme() {
+  Logger.log('git token,' + getGitToken())
+  var urlFetchOptions = {
+  "method": "GET",
+  "headers": {
+    "Accept": "application/vnd.github+json",
+    "X-GitHub-Api-Version": "2022-11-28",
+    "Authorization": `Bearer ${getGitToken()}`
+  },
+  "muteHttpExceptions": false
+  }
+  url = DOCUMENTS_URL + '/readme'
+  const gitResponse = UrlFetchApp.fetch(url, urlFetchOptions)
+  decodedResponse = decodeResponseBase64(gitResponse)
+  return decodedResponse
+}
+
+
+function putJson() {}
