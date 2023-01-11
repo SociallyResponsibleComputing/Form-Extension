008517721f671f76154ea51d0bd669761146758c
Alexander Pena <arpenacoding@gmail.com>
2023-01-11 11:23:42 -0800

    prettier styling and removing access token

--- scripts/Helpers.js
+++ scripts/Helpers.js
@@ -5,8 +5,13 @@ function printObj(obj) {
 }
 
 function getGitToken() {
-  PropertiesService.getScriptProperties().setProperty('gh_access_token', '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');
-  return PropertiesService.getUserProperties().getProperty('gh_access_token');
+  /*
+  PropertiesService.getScriptProperties().setProperty(
+    'gh_access_token',
+    'puttokenhere',
+  )
+  */
+  return PropertiesService.getUserProperties().getProperty('gh_access_token')
 }
 
 /* Parameters
@@ -16,13 +21,16 @@ function decodeResponseBase64(response) {
   const resStr = response.getContentText()
   const content64 = JSON.parse(resStr).content
   const byteContent = Utilities.base64Decode(content64, Utilities.Charset.UTF_8)
-  return Utilities.newBlob(byteContent).getDataAsString();
+  return Utilities.newBlob(byteContent).getDataAsString()
 }
 
 /* Parameters
   base64String: The base64 string to decode
 */
 function decodeBase64(base64String) {
-  const byteContent = Utilities.base64Decode(base64String, Utilities.Charset.UTF_8)
-  return Utilities.newBlob(byteContent).getDataAsString();
+  const byteContent = Utilities.base64Decode(
+    base64String,
+    Utilities.Charset.UTF_8,
+  )
+  return Utilities.newBlob(byteContent).getDataAsString()
 }
