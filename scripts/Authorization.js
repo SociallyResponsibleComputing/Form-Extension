0c7424d7ee57d6f08b4fe1c0bd5bb07243ff0602
AlexHappyCode <arpenacoding@gmail.com>
2023-02-15 17:37:49 -0800

    hard coded api key and prettier formatting

--- scripts/Authorization.js
+++ scripts/Authorization.js
@@ -1,30 +1,34 @@
 /* Helper Functions */
 
 /* Create UI menu for adding API keys */
-function TokenUI(){
+function TokenUI() {
   const ui = FormApp.getUi()
-   ui.createMenu('Credentials & Authentication')
+  ui.createMenu('Credentials & Authentication')
     .addItem('Set Github Access Token', 'setGithubAccessToken')
     .addItem('Delete API key', 'resetKey')
     .addItem('Delete all credentials', 'deleteAll')
-  .addToUi();
-} 
+    .addToUi()
+}
 
 function getGitToken() {
   // Logger.log(PropertiesService.getScriptProperties().getProperty('gh_access_token'))
-  return PropertiesService.getScriptProperties().getProperty('gh_access_token')
+  // return PropertiesService.getScriptProperties().getProperty('gh_access_token')
+  return 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
 }
 
 function setGithubAccessToken() {
   const ui = FormApp.getUi()
-  var scriptValue = ui.prompt('Please provide your API key.' , ui.ButtonSet.OK);
-  PropertiesService.getUserProperties().setProperty('gh_access_token', scriptValue.getResponseText());
+  var scriptValue = ui.prompt('Please provide your API key.', ui.ButtonSet.OK)
+  PropertiesService.getUserProperties().setProperty(
+    'gh_access_token',
+    scriptValue.getResponseText(),
+  )
 }
 
-function resetKey(){
-  PropertiesService.getUserProperties().deleteProperty('gh_access_token');
+function resetKey() {
+  PropertiesService.getUserProperties().deleteProperty('gh_access_token')
 }
 
-function deleteAll(){
-  PropertiesService.getUserProperties().deleteAllProperties();
+function deleteAll() {
+  PropertiesService.getUserProperties().deleteAllProperties()
 }
